d8b23abedfe25506c2693641dc89ab0e3281ea03
Teddi <teddi.orange@gmail.com>
2022-09-25 00:31:33 +0100

    comments comments comments!

--- src/tmpl/tmpl.go
+++ src/tmpl/tmpl.go
@@ -7,6 +7,7 @@ import (
 )
 
 func RenderPage(name string, path string, resp *http.ResponseWriter, data any) {
+	// Wouldn't usually handle templates like this, but for on the fly refreshing it's useful!
 	template.New(name)
 	returnedTemplate, _ := template.ParseFiles(path)
 
